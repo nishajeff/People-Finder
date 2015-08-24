d5de9c5379849e16aea41f8ed1c179da216b588c
NishaJeffrey <nishakpaul@gmail.com>
2015-08-24 13:46:45 -0400

    Final People Finder

--- PeopleFinder/src/PeopleFinder.java
+++ PeopleFinder/src/PeopleFinder.java
@@ -64,7 +64,7 @@ String url2="";
 	        message+="<div align=\"center\"><table style=\"border:2px solid black\">";
             message+="<th style=\" background-color:gray;border:2px solid black\">Title</th><th style=\" background-color:gray;border:2px solid black\">FirstName</th><th style=\" background-color:gray;border:2px solid black\">LastName</th><th style=\" background-color:gray;border:2px solid black\">Street</th><th style=\" background-color:gray;border:2px solid black\">Zip</th><th style=\" background-color:gray;border:2px solid black\">Email</th><th style=\" background-color:gray;border:2px solid black\">Position</th><th style=\" background-color:gray;border:2px solid black\">Company</th><th style=\" background-color:gray;border:2px solid black\">City</th><th style=\" background-color:gray;border:2px solid black\">State</th>";
 	        String query1="select c.fullname,c.title,c.fname,c.lname,c.street,c.zip,c.email,c.position,t.company_name,b.city_name,s.state_name from customer_bonus c , company_bonus t, cities b, states s where c.company_id=t.company_id and c.city_id=b.city_id and c.state_id= s.state_id and UPPER(c.lname)= UPPER('"+Name+"')";
-	        System.out.println(query1);
+	        //System.out.println(query1);
 	        res=s.executeQuery(query1);
 	        if(res.next()){
 		        
@@ -84,36 +84,27 @@ String url2="";
 		        }
 	        }
 	        else{
-	        	String query2="select c.fullname,c.title,c.fname,c.lname,c.street,c.zip,c.email,c.position,t.company_name,b.city_name,s.state_name from customer_bonus c , company_bonus t, cities b, states s where c.company_id=t.company_id and c.city_id=b.city_id and c.state_id= s.state_id  and upper(c.lname) like upper('"+Name+"%')  ";
-	        	res=s.executeQuery(query2);
-	        	 while (res.next()){
-		        	 message+="<tr ><td style=\" background-color:white;border:2px solid black\">"+ res.getString("title")+              
-		             		   "</td><td style=\" background-color:white;border:2px solid black\">"+res.getString("fname")+
-		             		   "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("lname")+
-		             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("street")+
-		             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getInt("zip")+
-		             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("email")+
-		             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("position")+
-		             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("company_name")+
-		             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("city_name")+
-		             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("state_name")+
-		             		  "</td></tr>" ;  
-		        }
-	        	String query3="select c.fullname,c.title,c.fname,c.lname,c.street,c.zip,c.email,c.position,t.company_name,b.city_name,s.state_name from customer_bonus c , company_bonus t, cities b, states s where c.company_id=t.company_id and c.city_id=b.city_id and c.state_id= s.state_id  and upper(t.company_name) like upper('%"+Name+"%')  ";
-	        	res=s.executeQuery(query3);
-	        	while (res.next()){
-	        	 message+="<tr ><td style=\" background-color:white;border:2px solid black\">"+ res.getString("title")+              
-	             		   "</td><td style=\" background-color:white;border:2px solid black\">"+res.getString("fname")+
-	             		   "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("lname")+
-	             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("street")+
-	             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getInt("zip")+
-	             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("email")+
-	             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("position")+
-	             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("company_name")+
-	             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("city_name")+
-	             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("state_name")+
-	             		  "</td></tr>" ;  
-	        }
+	        	
+		        	String query2="select cp.fullname,cp.title,cp.fname,cp.lname,cp.street,cp.zip,cp.email,cp.position,cp.company_name,cp.city_name,cp.state_name "+
+		        			"from (select c.fullname,c.title,c.fname,c.lname,c.street,c.zip,c.email,c.position,t.company_name,b.city_name,s.state_name "+
+		        					"from customer_bonus c , company_bonus t, cities b, states s "+
+		        					"where c.company_id=t.company_id and c.city_id=b.city_id and c.state_id= s.state_id) cp "+
+		        					"where upper(cp.lname) like upper('"+Name+"%')  or upper(cp.company_name) like upper('%"+Name+"%')";
+		        	res=s.executeQuery(query2);
+		        	 while (res.next()){
+			        	 message+="<tr ><td style=\" background-color:white;border:2px solid black\">"+ res.getString("title")+              
+			             		   "</td><td style=\" background-color:white;border:2px solid black\">"+res.getString("fname")+
+			             		   "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("lname")+
+			             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("street")+
+			             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getInt("zip")+
+			             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("email")+
+			             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("position")+
+			             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("company_name")+
+			             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("city_name")+
+			             		    "</td><td style=\"background-color:white;border:2px solid black\">" +res.getString("state_name")+
+			             		  "</td></tr>" ;  
+			        }
+	        	
 	        }
 		// set User object in request object and set URL
 		request.setAttribute("message", message);
